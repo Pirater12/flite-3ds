759598b5de3cd1075d07536c7b56a860811c29d8
Kartik <pokemonemerald0@gmail.com>
2016-11-27 12:28:22 +0530

    Add female voice

--- source/main.c
+++ source/main.c
@@ -2,9 +2,8 @@
 #include <stdio.h>
 #include <3ds.h>
 #include <flite/flite.h>
-
+cst_voice *register_cmu_us_slt();
 cst_voice *register_cmu_us_kal();
-
 ndspWaveBuf waveBuf;
 cst_wave *fliteWave;
 cst_voice *voice;
@@ -27,7 +26,7 @@ void processText(const char *text)
 	static int channel = 0;
 	int dataSize;
 	
-	printf("\n%s ", text);
+	printf("\x1b[37;1m\n%s\x1b[0m", text);
 	
 	fliteWave = flite_text_to_wave(text, voice);
 	dataSize = fliteWave->num_samples * fliteWave->num_channels * 2;
@@ -55,7 +54,11 @@ int main()
 {
 	gfxInitDefault();
 	ndspInit();
-	consoleInit(GFX_TOP, NULL);
+	PrintConsole top;
+	PrintConsole bottom;
+	consoleInit(GFX_TOP, &top);
+    consoleInit(GFX_BOTTOM, &bottom);
+	consoleSelect(&bottom);
 	
     SwkbdState swkbd;
     SwkbdButton button = SWKBD_BUTTON_NONE;
@@ -64,12 +67,35 @@ int main()
 	
 	waveBuf.status = NDSP_WBUF_DONE;
 	svcSleepThread(2000000000);
-    puts("Press A to begin");
+	printf("\x1b[31;1m");
+	printf("\x1b[12;8HWelcome to flite-3ds!");
+	printf("\x1b[32;1m");
+    printf("\x1b[13;8HPress A to begin");
+	printf("\x1b[33;1m");
+	printf("\x1b[14;8HPress R for female voice");
+	printf("\x1b[34;1m");
+	printf("\x1b[15;8HPress L for male voice\x1b[0m");
+	consoleSelect(&top);
 	while (aptMainLoop())
 	{
 		hidScanInput();
 		u32 kDown = hidKeysDown();
-
+        if(kDown & KEY_R)
+		{  
+			voice=register_cmu_us_slt(NULL);
+			consoleSelect(&bottom);
+			printf("\x1b[32;1m");
+			printf("\x1b[16;8HVoice changed to female\x1b[0m");
+			consoleSelect(&top);
+		}
+		if(kDown & KEY_L)
+		{
+			voice=register_cmu_us_kal(NULL);
+			consoleSelect(&bottom);
+			printf("\x1b[32;1m");
+			printf("\x1b[16;8HVoice changed to   male\x1b[0m");
+			consoleSelect(&top);
+		}
 		if (kDown & KEY_START)
 			break;
         
